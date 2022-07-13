295f3922128a365f0d56ea64a3dae2e7ceb1a047
Sebastian Kalas Moller <semo3108@icloud.com>
2022-07-13 19:39:52 +0200

    Add files via upload

---
+++ cdn/scripts/script.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+// Credits to CharveCompany
+function forcequit() {
+
+        if (confirm("Are you sure to exit? This might cause loss data!")) {
+            window.close();
+        }
+}
